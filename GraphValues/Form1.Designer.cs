424186283f969133c61962af88f0c57e3c4f377f
Zeno Rawling <zeno.rawling@gmail.com>
2013-03-25 21:51:18 +0000

    initial commit. draw a rectangle on a graph image we load

---
+++ GraphValues/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GraphValues
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.graphImage = new System.Windows.Forms.PictureBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.graphImage)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // graphImage
+            // 
+            this.graphImage.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.graphImage.Cursor = System.Windows.Forms.Cursors.Default;
+            this.graphImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.graphImage.Location = new System.Drawing.Point(0, 0);
+            this.graphImage.Name = "graphImage";
+            this.graphImage.Size = new System.Drawing.Size(1045, 656);
+            this.graphImage.TabIndex = 0;
+            this.graphImage.TabStop = false;
+            this.graphImage.Click += new System.EventHandler(this.graphImage_Click);
+            this.graphImage.Paint += new System.Windows.Forms.PaintEventHandler(this.graphImage_Paint);
+            this.graphImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseDown);
+            this.graphImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseMove);
+            this.graphImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseUp);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1045, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFileOpen,
+            this.menuFileExit});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(37, 20);
+            this.menuFile.Text = "File";
+            // 
+            // menuFileOpen
+            // 
+            this.menuFileOpen.Name = "menuFileOpen";
+            this.menuFileOpen.Size = new System.Drawing.Size(103, 22);
+            this.menuFileOpen.Text = "Open";
+            this.menuFileOpen.Click += new System.EventHandler(this.menuFileOpen_Click);
+            // 
+            // menuFileExit
+            // 
+            this.menuFileExit.Name = "menuFileExit";
+            this.menuFileExit.Size = new System.Drawing.Size(103, 22);
+            this.menuFileExit.Text = "Exit";
+            this.menuFileExit.Click += new System.EventHandler(this.menuFileExit_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1045, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.graphImage);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1045, 656);
+            this.panel1.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 705);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.graphImage)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox graphImage;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem menuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem menuFileExit;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ GraphValues/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphValues
+{
+    public partial class MainForm : Form
+    {
+        private Point _start = new Point();
+        bool _drawing = false;
+        Rectangle drawingRect;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void menuFileOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(open.FileName);
+                graphImage.Image = bmp;
+            }
+        }
+
+        private void menuFileExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void graphImage_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("clik");
+        }
+
+        private void graphImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            _start = e.Location;
+            drawingRect = new Rectangle(e.X, e.Y, 0, 0);
+            graphImage.Invalidate();
+            _drawing = true;
+        }
+
+        private void graphImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_drawing)
+            {
+                if (graphImage.Image == null)
+                    return;
+
+                drawingRect = new Rectangle(_start.X, _start.Y, Math.Max(_start.X, e.X) - Math.Min(_start.X, e.X), (Math.Max(_start.Y, e.Y) - Math.Min(_start.Y, e.Y)));
+
+                graphImage.Invalidate();
+            }
+        }
+
+        private void graphImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            _drawing = false;
+
+            //if (graphImage.Image == null)
+            //    return;
+
+            //using (Graphics g = Graphics.FromImage(graphImage.Image))
+            //{
+            //    g.DrawRectangle(Pens.Red, new Rectangle(_start.X, _start.Y, e.X - _start.X, e.Y - _start.Y));
+            //}
+            //graphImage.Invalidate();
+        }
+
+        private void graphImage_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(Pens.Red, drawingRect);
+        }
+    }
+}
