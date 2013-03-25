e17f566009f4d22c326325d7725bd7bfb93e79e8
Zeno Rawling <zeno.rawling@gmail.com>
2013-03-25 23:21:30 +0000

    get percentage value of point.x

--- GraphValues/Form1.Designer.cs
+++ GraphValues/Form1.Designer.cs
@@ -28,15 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.graphImage = new System.Windows.Forms.PictureBox();
             this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileExit = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonBoundary = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonPoint = new System.Windows.Forms.ToolStripButton();
             this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.graphImage)).BeginInit();
             this.menuStrip.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,8 +54,8 @@
             this.graphImage.Size = new System.Drawing.Size(1045, 656);
             this.graphImage.TabIndex = 0;
             this.graphImage.TabStop = false;
-            this.graphImage.Click += new System.EventHandler(this.graphImage_Click);
             this.graphImage.Paint += new System.Windows.Forms.PaintEventHandler(this.graphImage_Paint);
+            this.graphImage.MouseClick += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseClick);
             this.graphImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseDown);
             this.graphImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseMove);
             this.graphImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseUp);
@@ -91,11 +95,38 @@
             // 
             // toolStrip1
             // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonBoundary,
+            this.toolStripButtonPoint});
             this.toolStrip1.Location = new System.Drawing.Point(0, 24);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(1045, 25);
             this.toolStrip1.TabIndex = 2;
             this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonBoundary
+            // 
+            this.toolStripButtonBoundary.Checked = true;
+            this.toolStripButtonBoundary.CheckOnClick = true;
+            this.toolStripButtonBoundary.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripButtonBoundary.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonBoundary.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonBoundary.Image")));
+            this.toolStripButtonBoundary.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonBoundary.Name = "toolStripButtonBoundary";
+            this.toolStripButtonBoundary.Size = new System.Drawing.Size(79, 22);
+            this.toolStripButtonBoundary.Text = "I can haz axis";
+            this.toolStripButtonBoundary.Click += new System.EventHandler(this.toolStripButtonBoundary_Click);
+            // 
+            // toolStripButtonPoint
+            // 
+            this.toolStripButtonPoint.CheckOnClick = true;
+            this.toolStripButtonPoint.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonPoint.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonPoint.Image")));
+            this.toolStripButtonPoint.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonPoint.Name = "toolStripButtonPoint";
+            this.toolStripButtonPoint.Size = new System.Drawing.Size(100, 22);
+            this.toolStripButtonPoint.Text = "I can haz datums";
+            this.toolStripButtonPoint.Click += new System.EventHandler(this.toolStripButtonPoint_Click);
             // 
             // panel1
             // 
@@ -120,6 +151,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.graphImage)).EndInit();
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -135,6 +168,8 @@
         private System.Windows.Forms.ToolStripMenuItem menuFileExit;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonBoundary;
+        private System.Windows.Forms.ToolStripButton toolStripButtonPoint;
     }
 }
 
--- GraphValues/Form1.cs
+++ GraphValues/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace GraphValues
 {
     public partial class MainForm : Form
     {
         private Point _start = new Point();
+        bool _boundaryMode, _pointMode;
         bool _drawing = false;
+        
         Rectangle drawingRect;
+
+        List<Point> VisPoints = new List<Point>();
+        List<PointF> PercentPoints = new List<PointF>();
+
+        enum Mode {Boundary, Point};
 
         public MainForm()
         {
@@ -30,6 +38,7 @@ namespace GraphValues
                 Bitmap bmp = new Bitmap(open.FileName);
                 graphImage.Image = bmp;
             }
+            _boundaryMode = true;
         }
 
         private void menuFileExit_Click(object sender, EventArgs e)
@@ -37,27 +46,25 @@ namespace GraphValues
             Application.Exit();
         }
 
-        private void graphImage_Click(object sender, EventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine("clik");
-        }
-
         private void graphImage_MouseDown(object sender, MouseEventArgs e)
         {
-            _start = e.Location;
-            drawingRect = new Rectangle(e.X, e.Y, 0, 0);
-            graphImage.Invalidate();
-            _drawing = true;
+            if (_boundaryMode)
+            {
+                _start = e.Location;
+                drawingRect = new Rectangle(e.X, e.Y, 0, 0);
+                graphImage.Invalidate();
+                _drawing = true;
+            }
         }
 
         private void graphImage_MouseMove(object sender, MouseEventArgs e)
         {
-            if (_drawing)
+            if (_drawing && _boundaryMode)
             {
                 if (graphImage.Image == null)
                     return;
 
-                drawingRect = new Rectangle(_start.X, _start.Y, Math.Max(_start.X, e.X) - Math.Min(_start.X, e.X), (Math.Max(_start.Y, e.Y) - Math.Min(_start.Y, e.Y)));
+                drawingRect = new Rectangle(_start.X, _start.Y, Math.Max(_start.X, e.X) - Math.Min(_start.X, e.X), Math.Max(_start.Y, e.Y) - Math.Min(_start.Y, e.Y));
 
                 graphImage.Invalidate();
             }
@@ -65,21 +72,79 @@ namespace GraphValues
 
         private void graphImage_MouseUp(object sender, MouseEventArgs e)
         {
-            _drawing = false;
+            if (_drawing && _boundaryMode)
+            {
+                _drawing = false;
+                SetMode(Mode.Point);
 
-            //if (graphImage.Image == null)
-            //    return;
 
-            //using (Graphics g = Graphics.FromImage(graphImage.Image))
-            //{
-            //    g.DrawRectangle(Pens.Red, new Rectangle(_start.X, _start.Y, e.X - _start.X, e.Y - _start.Y));
-            //}
-            //graphImage.Invalidate();
+            }
         }
 
         private void graphImage_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.DrawRectangle(Pens.Red, drawingRect);
+
+            foreach (Point point in VisPoints)
+            {
+                e.Graphics.FillEllipse(Brushes.Red, point.X - 2, point.Y - 2, 4, 4);
+            }
+        }
+
+        private void toolStripButtonBoundary_Click(object sender, EventArgs e)
+        {
+            SetMode(Mode.Boundary);
+        }
+
+        private void toolStripButtonPoint_Click(object sender, EventArgs e)
+        {
+            SetMode(Mode.Point);
+        }
+
+        private void graphImage_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (_pointMode)
+            {
+                Point point = new Point(e.X, e.Y);
+                VisPoints.Add(point);
+                graphImage.Invalidate();
+
+                PercentPoints.Add(GetScalePoint(point));
+            }
+        }
+
+        private void SwitchModes()
+        {
+            _boundaryMode = !_boundaryMode;
+            toolStripButtonBoundary.Checked = _boundaryMode;
+
+            _pointMode = !_pointMode;
+            toolStripButtonPoint.Checked = _pointMode;
+        }
+
+        private void SetMode(Mode mode)
+        {
+            if (mode == Mode.Boundary)
+            {
+                _boundaryMode = toolStripButtonBoundary.Checked = true;
+                _pointMode = toolStripButtonPoint.Checked = false;
+            }
+            else
+            {
+                _pointMode = toolStripButtonPoint.Checked = true;
+                _boundaryMode = toolStripButtonBoundary.Checked = false;
+            }
+        }
+
+        private PointF GetScalePoint(Point visPoint)
+        {
+            int startOffset = drawingRect.X;
+            float relX = visPoint.X - startOffset;
+            Debug.WriteLine("relX, pointX, offset, width = " + relX + ", " + visPoint.X + ", " + startOffset + ", " + drawingRect.Width);
+            float percentX = (relX / (float)drawingRect.Width) * 100f;
+            System.Diagnostics.Debug.WriteLine("percentX = " + percentX);
+
+            return new PointF(percentX, 0f);
         }
     }
 }
